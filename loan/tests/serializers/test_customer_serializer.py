acea696f0bfd5d5a04512f72cffb20214cb4c28a
Bianka Passos <biankatpas@gmail.com>
2024-09-04 18:41:23 -0300

    tests for customer serializer

---
+++ loan/tests/serializers/test_customer_serializer.py
@@ -0,0 +1,28 @@
+import pytest
+
+from rest_framework.exceptions import ValidationError
+
+from loan.serializers import CustomerSerializer
+from loan.tests.fixtures.customer_fixtures import create_customer
+
+
+@pytest.mark.django_db
+def test_customer_serializer_valid_data(create_customer):
+    customer = create_customer
+
+    serializer = CustomerSerializer(instance=customer)
+
+    assert serializer.data['name'] == customer.name
+    assert serializer.data['id'] == str(customer.id)
+
+
+@pytest.mark.django_db
+def test_customer_serializer_invalid_data():
+    mock_invalid_data = {
+        'name': '',
+    }
+
+    serializer = CustomerSerializer(data=mock_invalid_data)
+
+    assert not serializer.is_valid()
+    assert 'name' in serializer.errors
