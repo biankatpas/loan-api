8feee33a1ab2487642dc7dd52ed4a29b3eb8063a
Bianka Passos <biankatpas@gmail.com>
2024-09-05 15:25:00 -0300

    tests for authentication

---
+++ loan/tests/authentication/test_authentication.py
@@ -0,0 +1,89 @@
+import pytest
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+@pytest.mark.django_db
+def test_token_obtain_pair():
+    client = APIClient()
+
+    mock_username = 'testuser'
+    mock_password = 'testpassword'
+
+    _ = User.objects.create_user(username=mock_username, password=mock_password)
+
+    response = client.post(
+        '/api/token/',
+        {
+            'username': mock_username,
+            'password': mock_password,
+        },
+        format='json'
+    )
+
+    assert response.status_code == 200
+    assert 'access' in response.data
+    assert 'refresh' in response.data
+
+
+@pytest.mark.django_db
+def test_token_does_not_obtain_pair():
+    client = APIClient()
+
+    mock_username = 'testuser'
+    mock_password = 'testpassword'
+
+    _ = User.objects.create_user(username=mock_username, password=mock_password)
+
+    response = client.post(
+        '/api/token/',
+        {
+            'username': mock_username,
+            'password': 'wrongpassword'
+        },
+        format='json'
+    )
+
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_token_refresh():
+    client = APIClient()
+
+    mock_username = 'testuser'
+    mock_password = 'testpassword'
+
+    user = User.objects.create_user(username=mock_username, password=mock_password)
+    refresh = RefreshToken.for_user(user)
+
+    response = client.post(
+        '/api/token/refresh/',
+        {'refresh': str(refresh)},
+        format='json'
+    )
+
+    assert response.status_code == 200
+    assert 'access' in response.data
+
+    response = client.post(
+        '/api/token/refresh/',
+        {'refresh': 'invalidtoken'},
+        format='json'
+    )
+
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_token_does_not_refresh():
+    client = APIClient()
+
+    response = client.post(
+        '/api/token/refresh/',
+        {'refresh': 'invalidtoken'},
+        format='json'
+    )
+
+    assert response.status_code == 401
