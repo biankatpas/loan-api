6839bf7a6b531a697d452866d5de56e2032d94a5
Bianka Passos <biankatpas@gmail.com>
2024-09-08 18:39:52 -0300

    added tests for payment serializer

--- loan/tests/serializers/test_payment_serializer.py
+++ loan/tests/serializers/test_payment_serializer.py
@@ -1,8 +1,11 @@
+import uuid
+from unittest.mock import MagicMock
+
 import pytest
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 
-from loan.serializers import PaymentSerializer
+from loan.serializers import LoanDetailSerializer, PaymentSerializer
 from loan.tests.fixtures.payment_fixtures import (create_bank, create_customer,
                                                   create_loan, create_payment,
                                                   create_user)
@@ -20,6 +23,19 @@ def test_payment_serializer_valid_data(create_payment):
 
 
 @pytest.mark.django_db
+def test_payment_serializer_representation(create_payment):
+    payment = create_payment
+
+    serializer = PaymentSerializer(instance=payment)
+
+    serialized_data = serializer.data
+
+    assert serializer.data["amount"] == str(payment.amount)
+    assert serializer.data["payment_date"] == str(payment.payment_date)
+    assert serialized_data["loan"] == LoanDetailSerializer(payment.loan).data
+
+
+@pytest.mark.django_db
 def test_payment_serializer_invalid_data(create_payment):
     payment = create_payment
 
@@ -33,3 +49,34 @@ def test_payment_serializer_invalid_data(create_payment):
 
     assert not serializer.is_valid()
     assert "amount" in serializer.errors
+
+
+@pytest.mark.django_db
+def test_payment_serializer_customer_validation(create_payment):
+    payment = create_payment
+
+    invalid_loan_id = str(uuid.uuid4())
+
+    mock_request = MagicMock()
+    mock_request.user = payment.loan.customer.user
+
+    serializer = PaymentSerializer(instance=payment, context={"request": mock_request})
+
+    with pytest.raises(serializers.ValidationError) as error_detail:
+        serializer.validate_loan(invalid_loan_id)
+        assert "Loan does not exist." in str(error_detail.value)
+
+
+@pytest.mark.django_db
+def test_payment_serializer_permission_denied(create_payment):
+    payment = create_payment
+
+    mock_request = MagicMock()
+    mock_request.user = MagicMock()
+    mock_request.user.id = str(uuid.uuid4())
+
+    serializer = PaymentSerializer(instance=payment, context={"request": mock_request})
+
+    with pytest.raises(serializers.ValidationError) as error_detail:
+        serializer.validate_loan(payment.loan.id)
+        assert "Loan does not belong to the current user." in str(error_detail.value)
